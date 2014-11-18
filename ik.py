e7b5813695fbfac9f87c32528c2606fc97ca18d5
lcruz4 <lcruz4@github.com>
2014-11-18 13:38:20 -0500

    jacobian transposed

--- ik.py
+++ ik.py
@@ -7,7 +7,7 @@ from math import *
 from ctypes import *
 from matscript import *
 
-def jacobian(xyz0,xyz1,xyz2,xyz3,delta):
+'''def jacobian(xyz0,xyz1,xyz2,xyz3,delta):
   j00 = (xyz1[0] - xyz0[0])/delta
   j01 = (xyz1[1] - xyz0[1])/delta
   j02 = (xyz1[2] - xyz0[2])/delta
@@ -16,6 +16,20 @@ def jacobian(xyz0,xyz1,xyz2,xyz3,delta):
   j12 = (xyz2[2] - xyz0[2])/delta
   j20 = (xyz3[0] - xyz0[0])/delta
   j21 = (xyz3[1] - xyz0[1])/delta
+  j22 = (xyz3[2] - xyz0[2])/delta
+  return np.matrix([[j00,j01,j02],
+                    [j10,j11,j12],
+                    [j20,j21,j22]])
+'''
+def jacobian(xyz0,xyz1,xyz2,xyz3,delta):
+  j00 = (xyz1[0] - xyz0[0])/delta
+  j01 = (xyz2[0] - xyz0[0])/delta
+  j02 = (xyz3[0] - xyz0[0])/delta
+  j10 = (xyz1[1] - xyz0[1])/delta
+  j11 = (xyz2[1] - xyz0[1])/delta
+  j12 = (xyz3[1] - xyz0[1])/delta
+  j20 = (xyz1[2] - xyz0[2])/delta
+  j21 = (xyz2[2] - xyz0[2])/delta
   j22 = (xyz3[2] - xyz0[2])/delta
   return np.matrix([[j00,j01,j02],
                     [j10,j11,j12],
@@ -45,10 +59,9 @@ aftarm = -.17914
 rsr = 0
 reb = 0
 rsp = 0
-xyzd = [-.25,-.2,.2]
+xyzd = [-.25,-.2,-.2]
 xdone = False
 ydone = False
-x=0
 xyz0 = fk(aftarm,forearm,0,0,0)
 xyz0[0] += shoulder
 xyz = xyzSplit(xyzd,xyz0,100)
@@ -66,6 +79,10 @@ for i in range(100):
                   [xyz[i,1]-xyz0[1]],
                   [xyz[i,2]-xyz0[2]]])
   dtheta = J.getI()*de
+  print 'Jacobian'
+  print J
+  print 'inverse'
+  print J.getI()
   print 'current position'
   print xyz0
   print 'desired step position'
@@ -80,16 +97,16 @@ for i in range(100):
   ref.ref[ha.RSR] = -rsr+pi/12
   ref.ref[ha.REB] = -reb+pi/18
   ref.ref[ha.RSP] = -rsp+pi/18
-  r.put(ref)
-  [statuss, framesizes] = s.get(state, wait=False, last=False)
+#  r.put(ref)
+  try:
+    input("Press enter to continue")
+  except SyntaxError:
+    pass
+'''  [statuss, framesizes] = s.get(state, wait=False, last=False)
   t=state.time
   while((state.time-t)<.1):
     [statuss, framesizes] = s.get(state, wait=False, last=False) 
-  #try:
-  #  input("Press enter to continue")
-  #except SyntaxError:
-  #  pass
-  x += 1
+'''
 r.close()
 s.close()
 
